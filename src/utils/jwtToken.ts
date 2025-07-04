063086a2442692cce8e05cdfca5ad57057df774e
clear <oopaletijohnson@gmail.com>
2025-07-05 00:07:02 +0100

    fifth commit

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -6,7 +6,9 @@ const sendToken = (user: any, statusCode: number, res: Response): string => {
   res.cookie("token", token, {
     httpOnly: true, // more secure
     maxAge: 1 * 24 * 60 * 60 * 1000, // 15 days
-    sameSite: "strict", // CSRF
+    // sameSite: "strict", // CSRF
+    sameSite: "lax", // allow cross-site POST
+    secure: false, // allow cookies over HTTP
   });
 
   return token;
